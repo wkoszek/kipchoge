5f15d578f9893e9197d7455e26ef7f5c861479da
Wojciech Adam Koszek <wojciech@koszek.com>
2017-05-08 21:51:07 -0700

    Bring the date processing

--- kipchoge.rb
+++ kipchoge.rb
@@ -2,6 +2,7 @@
 # vim:tw=1000:
 
 require 'erb'
+require 'time'
 require 'yaml'
 require 'json'
 require 'pp'
@@ -17,9 +18,12 @@ class Article
 
   def initialize(filename, blog)
     @filename = filename
+    @filename_base = File.basename(filename)
     @data_raw = File.read(@filename)
     @data_tmp = parse_data(@data_raw)
     @data_tmp['filename'] = filename
+    @data_tmp['filename_base'] = @filename_base
+    @data_tmp['written_date'] = date_from_filename(@filename_base)
     @data_tmp['render_time'] = Time.new
     @data = OpenStruct.new(@data_tmp)
     @blog = blog
@@ -37,6 +41,15 @@ class Article
       ret['article_body'] = data_raw
     end
     ret
+  end
+
+  def date_from_filename(filename)
+    d = filename[0..'yyyy-mm-dd'.length]
+    if filename =~ /^\d{4}-\d{2}-\d{2}/
+      Time.parse(filename[0..'yyyy-mm-dd'.length])
+    else
+      Time.parse('1970-01-01')
+    end
   end
 
   def filename_output(cfg)
@@ -76,8 +89,10 @@ class Blog
     renderer.result(obj.get_binding)
   end
   def add_all
-    Dir["#{@cfg.dirs.source}/**/*.md"].each do |dir_entry|
-      #STDERR.puts "> rendering #{dir_entry}"
+    cfg_dirs = "#{@cfg.dirs.source}/#{@cfg.dirs.pattern}"
+    STDERR.puts ">>", cfg_dirs
+    Dir[cfg_dirs].each do |dir_entry|
+      STDERR.puts "> rendering #{dir_entry}"
       article_one = Article.new(dir_entry, self)
       add(article_one)
     end
@@ -89,8 +104,11 @@ class Blog
       fn_out = a.filename_output(@cfg)
       dir_out = File.dirname(fn_out)
       STDERR.puts "using layout: #{layout_file} FOUT #{fn_out} DIROUT #{dir_out}"
+
+      rendered_body = render(layout_file, a)
+      body_to_write = rendered_body
       FileUtils.mkdir_p(dir_out)
-      File.write(fn_out, render(layout_file, a))
+      File.write(fn_out, body_to_write)
     end
   end
 end
